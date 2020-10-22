ab1e21f4c71ec065f85d33fe719c1d0d1e45563b
alltomasterpiece <alltomasterpiece@gmail.com>
2020-10-22 16:06:57 -0700

    voice record button with css and js

---
+++ app/code/VoiceQr/Record/Block/record.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VoiceQr\Record\Block;
+
+use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \Toptal\Blog\Model\ResourceModel\Post\Collection as PostCollection;
+use \Toptal\Blog\Model\ResourceModel\Post\CollectionFactory as PostCollectionFactory;
+
+
+class Record extends Template
+{
+ 
+}
---
+++ app/code/VoiceQr/Record/view/frontend/web/js/record.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var recordButton = document.getElementById("recordButton");
+    recordButton.addEventListener("click", startRecording);
+    function startRecording() {
+    console.log("recordButton clicked");		
+    }
+};
+        
+
